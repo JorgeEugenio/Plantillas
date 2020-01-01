761c6759bc989fc3fa31b36999ea50b6252aff20
JorgeEugenio <er.jorgeluis@gmail.com>
2020-01-01 17:39:23 -0500

    documentacion de mongoDB

---
+++ MongoDB/2_documentos.js
@@ -0,0 +1,16 @@
+//nucleo de datos de mongodb son los documentos
+/*{
+    _id:123456,
+    firstName:"Julio",
+    lastName:"Grados" 
+}
+{
+    _id:1234456,
+    names:{
+        firstName:"Julio",
+        lastName:"Grados" 
+    },
+    single:true
+    roles:['user','superuser']
+}
+*/
